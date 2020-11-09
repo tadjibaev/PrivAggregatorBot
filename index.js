af07e69390e95a3e394ac39a4faf8a2d106e6651
Kamariddin Sharapov <tadjibaev.ulugbek@gmail.com>
2020-11-10 02:35:22 +0500

    test commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api');
-
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 80;
-``
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
